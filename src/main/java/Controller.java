60e69bb783f1ebc16999536ea1e41151f4bad5c1
ericpalmas <ericpalmas96@gmail.com>
2019-02-26 13:35:42 +0100

    Update - matrice di adiacenza

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -5,11 +5,6 @@ public class Controller {
     public static ArrayList<City> cities = new ArrayList<>();
     private static Integer[][] adjacencyMatrix;
 
-    public static int distFrom(double lat1, double lng1, double lat2, double lng2) {
-        int dist = (int) Math.sqrt(Math.pow(lat2-lat1,2)+Math.pow(lng2-lng1,2));
-        return dist;
-    }
-
 
     public static void main(String[] args){
 
@@ -20,22 +15,19 @@ public class Controller {
 
     }
 
+    public static int distFrom(double lat1, double lng1, double lat2, double lng2) {
+        int dist = (int) (Math.sqrt(Math.pow(lat2-lat1,2)+Math.pow(lng2-lng1,2)) + 0.5);
+        return dist;
+    }
 
     private static void calculateDistance() {
         for(int i=0;i<cities.size();i++){
             for(int j=0;j<cities.size();j++){
-                if(i==j){
-                    adjacencyMatrix[i][j] = Integer.MAX_VALUE;
-                }else
+//                if(i==j){
+//                    adjacencyMatrix[i][j] = Integer.MAX_VALUE;
+//                }else
                     adjacencyMatrix[i][j] = distFrom(cities.get(i).getLatitude(),cities.get(i).getLongitude(),cities.get(j).getLatitude(),cities.get(j).getLongitude());
             }
         }
-
-//        for(int i=0;i<cities.size();i++){
-//            for(int j=0;j<cities.size();j++){
-//                System.out.print(adjacencyMatrix[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
     }
 }
