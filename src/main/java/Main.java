d7d1c855682cd5885532f47c30e37c91c44efc02
ericpalmas <ericpalmas96@gmail.com>
2019-03-30 15:07:23 +0100

    Update - simulated annealing funzionante

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,110 +1,107 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
 
+//    public static int getTourDistance(int[] path, DistanceMatrix distanceMatrix) {
+//        int distance = 0;
+//        for (int i = 0; i < path.length - 1; i++) {
+//            distance += distanceMatrix.getDistanceMatrix()[path[i]][path[i + 1]];
+//        }
+//        return distance;
+//    }
 
-    public static int getTourDistance(int[] path, DistanceMatrix distanceMatrix) {
+    public static Long[] seeds;
 
-        int distance = 0;
-        for (int i = 0; i < path.length - 1; i++) {
-            distance += distanceMatrix.getDistanceMatrix()[path[i]][path[i + 1]];
-        }
-        return distance;
-    }
-
-    public static void printCoordinates(int[] path, ArrayList<City> cities){
-        System.out.println("COORDINATE\n");
-        System.out.println("Latitudine:");
-
-
-        for(int i=0;i<path.length;i++){
-            System.out.println(cities.get(path[i]).getLatitude().toString().replace('.',','));
-        }
-        System.out.println("Longitudine: \n\n");
-        for (int j=0;j<path.length;j++){
-            System.out.println(cities.get(path[j]).getLongitude().toString().replace('.',','));
-        }
-        System.out.println("\n\n\n");
-    }
-
-    public static long[] seeds = {1553612458630L,1553723993577L,1553723997181L,1553724163894L,
-            1553521360686L,1553724172912L,1553724210825L,1553724229277L,1553724306305L,1553724443917L};
-
-    public static double[] costs = {6310,16980,548,23249,22282,43029,51778,108217,8906,234094};
+    public static Integer[] costs;
 
     public static int[] bestKnowns = {6110,15780,538,22249,21282,42029,50778,107217,8806,224094};
 
     public static   String[] files = {"ch130.tsp", "d198.tsp", "eil76.tsp", "fl1577.tsp"
                     , "kroA100.tsp", "lin318.tsp", "pcb442.tsp"
                 , "pr439.tsp", "rat783.tsp", "u1060.tsp"};
-    public static boolean migliorato = false;
+
+    public static boolean improve = false;
+
     public static void main(String[] args) {
+    costs = new Integer[10];
+    seeds = new Long[10];
 
         while(true){
+            readSeeds();
             for (int i=0;i<10;i++){
-                System.out.println(files[i]+"\n");
+                System.out.println(files[i]+"\n\n");
                 TspReader tspReader = new TspReader(files[i]);
                 ArrayList<City> cities = tspReader.getCities();
                 DistanceMatrix distanceMatrix = new DistanceMatrix(cities);
                 long startTime = System.nanoTime();
 
-                //questo metodo è solo per la ricerca dei seeds
                 Random random = new Random();
-                //random.setSeed(System.currentTimeMillis());
-                random.setSeed(200L);
+                long seed = System.currentTimeMillis();
+                random.setSeed(seed);
 
                 //Algoritmo costruttivo
                 NearestNeighbor neirestNeighbor = new NearestNeighbor(cities, distanceMatrix,random);
                 int[] path = neirestNeighbor.calculate();
-                System.out.println("\nNN -> Errore relativo = " + ((getTourDistance(path,distanceMatrix)  - tspReader.getBestKnown()) / tspReader.getBestKnown()) * 100 + " %");
-                System.out.println("dis: " + getTourDistance(path,distanceMatrix));
 
                 //Algoritmo euristico
                 TwoOpt twoOpt = new TwoOpt(path, distanceMatrix);
-                int[] newTour = twoOpt.optimize();
-                System.out.println("\nTO -> Errore relativo = " + (( getTourDistance(path,distanceMatrix) - tspReader.getBestKnown()) / tspReader.getBestKnown()) * 100 + " %");
-                System.out.println("dis: " + getTourDistance(newTour,distanceMatrix));
+                int[] path2 = twoOpt.optimize();
+
 
                 //Algoritmo meta-euristico
-                SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(newTour, distanceMatrix, startTime, random);
+                SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(path2, distanceMatrix, startTime, random);
                 int[] finalTour = simulatedAnnealing.compute();
                 int dist = simulatedAnnealing.getTourDistance(finalTour);
-                System.out.println("\nSA -> Errore relativo = " + (( getTourDistance(finalTour,distanceMatrix) - tspReader.getBestKnown()) / tspReader.getBestKnown()) * 100 + " %");
-                System.out.println("dist final " + getTourDistance(finalTour,distanceMatrix));
+                System.out.println("dist " + dist);
+                System.out.println("\nSA -> Errore relativo = " + ((dist - (double)bestKnowns[i]) / (double)bestKnowns[i]) * 100 + " %");
+
 
                 if(dist<costs[i]){
                     costs[i] = dist;
-                    seeds[i] = tspReader.getSeed();
-                    System.out.println(files[i] + "migliorato\n");
-                    migliorato = true;
+                    seeds[i] = seed;
+                    improve = true;
                 }
 
                 long endTime = System.nanoTime();
                 long totalTime = endTime - startTime;
-                System.out.println("\nExecution time: " + totalTime * 1.6667e-11 + " minutes");
-
+                System.out.println("\nExecution time: " + totalTime * 1.6667e-11 + " minutes\n");
             }
-            if(migliorato)
+            if(improve)
                writeSeeds();
         }
     }
 
     private static void writeSeeds() {
         try {
-            BufferedWriter out = new BufferedWriter(new FileWriter("seeds.txt",true));
+            BufferedWriter out = new BufferedWriter(new FileWriter("seeds.txt"));
             for (int i=0;i<seeds.length;i++) {
-                out.write(files[i] + ", SEED: " + seeds[i] + ", COST: "+ costs[i] + ", ERROR: " + (((costs[i]-(double)bestKnowns[i])/(double)bestKnowns[i])*100+ " %") + "\n");
-                seeds[i]= System.currentTimeMillis();
+                out.write(files[i] + "," + seeds[i] + ","+ costs[i] + "," + (((costs[i]-(double)bestKnowns[i])/(double)bestKnowns[i])*100+ " %") + "\n");
             }
             out.write("\n\n\n");
             out.flush();
             out.close();
-            migliorato = false;
+            improve = false;
         }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void readSeeds(){
+        BufferedReader reader;
+        String[] informations;
+        try {
+            reader = new BufferedReader(new FileReader("seeds.txt"));
+            String line;
+             for(int i=0;i<10;i++) {
+                 line = reader.readLine();
+                 informations = line.split(",");
+                 seeds[i] = Long.parseLong(informations[1]);
+                 costs[i] = Integer.parseInt(informations[2]);
+             }
+            reader.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/SimulatedAnnealing.java
+++ src/main/java/SimulatedAnnealing.java
@@ -23,134 +23,6 @@ public class SimulatedAnnealing {
         return distance;
     }
 
-//    public int[] computeSolution() {
-//        double temperature = 100, alpha = 0.95;
-//        int[] candidate, next, current = path.clone(), best = current.clone();
-//        long end;
-//
-//        do {
-//            for (int i = 0; i < 100; i++) {
-//
-//                next = executeDoubleBridge(current);
-//
-//                TwoOpt twoOpt = new TwoOpt(next, distanceMatrix);
-//                candidate = twoOpt.optimize().clone();
-//
-//                if (getTourDistance(candidate) < getTourDistance(current)) {
-//                    current = candidate.clone();
-//                    if (getTourDistance(current) < getTourDistance(best)) {
-//                        best = current.clone();
-//                        //System.out.println("SA: " + getTourDistance(best) + " seed: " + System.currentTimeMillis());
-//                    }
-//                } else if (random.nextDouble() < Math.exp(-(getTourDistance(candidate) - getTourDistance(current)) / temperature)) {
-//                    current = candidate.clone();
-//                }
-//                end = System.nanoTime();
-//                if (end - startTime > 1.71e+11)
-//                    return best;
-//            }
-//            temperature *= alpha;
-//
-//        } while (temperature > 0.00001);
-//        return best;
-//    }
-
-
-//    public int[] computeSolution() {
-//        double temperature = 100, alpha = 0.95;
-//        int[] candidate, current = path.clone(), best = current.clone();
-//        long end;
-//
-//
-//        do {
-//            for (int i = 0; i < 100; i++) {
-//
-//                currentDistance = executeDoubleBridge(current);
-//
-//                TwoOpt twoOpt = new TwoOpt(path, distanceMatrix);
-//                candidate = twoOpt.optimize().clone();
-//
-//                candidateDistance = getTourDistance(candidate);
-//                if (candidateDistance < currentDistance) {
-//                    current = candidate.clone();
-//                    if (candidateDistance < getTourDistance(best)) {
-//                        best = current.clone();
-//                        currentDistance = getTourDistance(best);
-//                        System.out.println("SA: " + getTourDistance(best) + " seed: " + System.currentTimeMillis());
-//                    }
-//                } else if (random.nextDouble() < Math.exp(-((double) candidateDistance - currentDistance) / temperature)) {
-//                    current = candidate.clone();
-//                }
-//                end = System.nanoTime();
-//                if (end - startTime > 1.71e+11)
-//                    return best;
-//            }
-//            temperature *= alpha;
-//
-//        } while (temperature > 0.0001);
-//        return best;
-//    }
-
-//    private int executeDoubleBridge(int[] path) {
-//        int tourDistance = getTourDistance(path);
-//        int pos1, pos2, pos3, pos4;
-//        do {
-//            pos1 = random.nextInt(path.length - 1);
-//            pos2 = random.nextInt(path.length - 1);
-//            pos3 = random.nextInt(path.length - 1);
-//            pos4 = random.nextInt(path.length - 1);
-//        } while (pos1 < 0 || pos2 < pos1 || pos3 < pos2 || pos4 < pos3);
-//
-//        int gain = calculateGain(pos1,pos2,pos3,pos4);
-//        int[] temp_solution = path.clone();
-//
-//        int k = pos1 + 1;
-//
-//        for (int j = pos3 + 1; j <= pos4; j++, k++)
-//            path[k] = temp_solution[j];
-//        for (int j = pos2 + 1; j <= pos3; j++, k++)
-//            path[k] = temp_solution[j];
-//        for (int j = pos1 + 1; j <= pos2; j++, k++)
-//            path[k] = temp_solution[j];
-//
-//        return tourDistance + gain;
-//    }
-
-
-//    private int[] executeDoubleBridge(int[] path) {
-//        int pos1, pos2, pos3, pos4;
-//        do {
-//            pos1 = random.nextInt(path.length - 1);
-//            pos2 = random.nextInt(path.length - 1);
-//            pos3 = random.nextInt(path.length - 1);
-//            pos4 = random.nextInt(path.length - 1);
-//        } while (pos1 < 0 || pos2 < pos1 || pos3 < pos2 || pos4 < pos3);
-//
-//        int[] temp_solution = path.clone();
-//
-//        int k = pos1 + 1;
-//
-//        for (int j = pos3 + 1; j <= pos4; j++, k++)
-//            path[k] = temp_solution[j];
-//        for (int j = pos2 + 1; j <= pos3; j++, k++)
-//            path[k] = temp_solution[j];
-//        for (int j = pos1 + 1; j <= pos2; j++, k++)
-//            path[k] = temp_solution[j];
-//
-//        return path;
-//    }
-
-    private int calculateGain(int pos1, int pos2, int pos3, int pos4) {
-        return distanceMatrix.getDistanceMatrix()[path[pos1]][path[pos3 + 1]]
-                + distanceMatrix.getDistanceMatrix()[path[pos4]][path[pos2 + 1]]
-                + distanceMatrix.getDistanceMatrix()[path[pos3]][path[pos1 + 1]]
-                + distanceMatrix.getDistanceMatrix()[path[pos2]][path[pos4 + 1]]
-                -distanceMatrix.getDistanceMatrix()[path[pos1]][path[pos1 + 1]]
-                - distanceMatrix.getDistanceMatrix()[path[pos2]][path[pos2 + 1]]
-                - distanceMatrix.getDistanceMatrix()[path[pos3]][path[pos3 + 1]]
-                - distanceMatrix.getDistanceMatrix()[path[pos4]][path[pos4 + 1]];
-    }
-
     public int[] compute() {
         double temperature = 100, alpha = 0.95;
         int[] next,candidate,current = path.clone();
@@ -195,41 +67,14 @@ public class SimulatedAnnealing {
             pos4 = random.nextInt(num_cities);
         } while (pos1 < 0 || pos2 < pos1 || pos3 < pos2 || pos4 < pos3);
 
-
-        final int incPos1 = pos1 + 1;
-        final int incPos2 = pos2 + 1;
-        final int incPos3 = pos3 + 1;
-//        final int incPos4 = pos4 + 1;
-
-//        final int sub = -distanceMatrix.getDistanceMatrix()[array[pos1]][array[incPos1]]
-//                - distanceMatrix.getDistanceMatrix()[array[pos2]][array[incPos2]]
-//                - distanceMatrix.getDistanceMatrix()[array[pos3]][array[incPos3]]
-//                - distanceMatrix.getDistanceMatrix()[array[pos4]][array[incPos4]];
-//
-//        final int add = +distanceMatrix.getDistanceMatrix()[array[pos1]][array[incPos3]]
-//                + distanceMatrix.getDistanceMatrix()[array[pos4]][array[incPos2]]
-//                + distanceMatrix.getDistanceMatrix()[array[pos3]][array[incPos1]]
-//                + distanceMatrix.getDistanceMatrix()[array[pos2]][array[incPos4]];
-//        SimulatedAnnealing.bestValue += (add + sub);
-
         int[] temp_solution = array.clone();
-        int k = incPos1;
+        int k = pos1 + 1;
 
-
-//        for (int j = incPos3; j <= pos4; j++, k++)
-//            array[k] = temp_solution[j];
-//        for (int j = incPos2; j <= pos3; j++, k++)
-//            array[k] = temp_solution[j];
-//        for (int j = incPos1; j <= pos2; j++, k++)
-//            array[k] = temp_solution[j];
-
-        //System.arraycopy(source_arr,sourcePos,dest_arr,destPos,len);
-
-        System.arraycopy(temp_solution,k,array,incPos3,pos4+1-incPos3);
-        k+=pos4+1-incPos3;
-        System.arraycopy(temp_solution,k,array,incPos2,pos3+1-incPos2);
-        k+=pos3+1-incPos2;
-        System.arraycopy(temp_solution,k,array,incPos1,pos2+1-incPos1);
+        System.arraycopy(temp_solution,k,array,pos3 + 1,pos4-pos3);
+        k+=pos4-pos3;
+        System.arraycopy(temp_solution,k,array,pos2 + 1,pos3-pos2);
+        k+=pos3-pos2;
+        System.arraycopy(temp_solution,k,array,pos1 + 1,pos2-pos1);
         return array;
     }
 }
